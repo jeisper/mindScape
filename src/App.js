909e39b29f7f4d5f185155ad3b37712dc5ca3702
jeisper <71713717+jeisper@users.noreply.github.com>
2023-04-12 12:31:43 +0100

    setup of router and changes on practice section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ChakraProvider, theme, Flex } from '@chakra-ui/react';
 import Landing from './pages/Landing';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Flex>
-        <Landing />
-      </Flex>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/evaluation" element={<Landing />} />
+          <Route path="/practice" element={<Landing />} />
+          <Route path="/practice/:id" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
