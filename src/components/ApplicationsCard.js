8ee13366facac95e6cd1a22c79df75f89ee6896c
jeisper <71713717+jeisper@users.noreply.github.com>
2023-04-12 15:55:31 +0100

    completed main content page with search bar and navegation

--- src/components/ApplicationsCard.js
+++ src/components/ApplicationsCard.js
@@ -1,10 +1,31 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ApplicationsCard() {
+  const navigate = useNavigate();
   return (
-    <Flex h="250px" bg="blue.200" p="4" m="10" borderRadius="20" boxShadow="lg">
-      ApplicationsCard
+    <Flex
+      h="250px"
+      bgGradient="linear(to-b, orange.200, orange.500)"
+      p="4"
+      m="10"
+      borderRadius="20"
+      boxShadow="lg"
+      display="flex"
+      flexDir="column"
+    >
+      <Text fontWeight="bold" fontSize="40">
+        Career
+      </Text>
+      <Flex display="flex" flexDir="row" ml="auto" mt="auto">
+        <Button mx="2" onClick={() => navigate('/generator')}>
+          CV Generator
+        </Button>
+        <Button mx="2" onClick={() => navigate('/eval')}>
+          CV Evaluator
+        </Button>
+      </Flex>
     </Flex>
   );
 }
